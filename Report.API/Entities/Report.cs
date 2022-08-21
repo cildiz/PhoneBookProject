15f43073cede52a01cf2c242cc186ad45c9a9608
Can ILDIZ <canildiz@outlook.com>
2022-08-21 20:34:13 +0300

    Rapor.API mikroservisi için gerekli Entity modelleri oluşturuldu.

---
+++ Report.API/Entities/Report.cs
@@ -0,0 +1,14 @@
+﻿using Report.API.Entities.Base;
+using Report.API.Enumerables;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Report.API.Entities
+{
+    [Table("Reports")]
+    public class Report : BaseEntity
+    {
+        public DateTime Date { get; set; }
+        public ReportStatus ReportStatus { get; set; }
+        public virtual List<ReportDetail>? ReportDetails { get; set; }
+    }
+}
---
+++ Report.API/Enumerables/ReportStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Report.API.Enumerables
+{
+    public enum ReportStatus
+    {
+        [Description("Hazırlanıyor")]
+        Preparing,
+        [Description("Tamamlandi")]
+        Completed
+    }
+}
